5beaa79f724109d45831969e222b4ef11c96e0a2
Ghanem99 <moghanem345@gmail.com>
2024-04-15 01:48:27 +0200

    Use breeze layout instead!

--- resources/views/layout/navbar.blade.php
+++
@@ -1,4 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Your Project</a>
-    <!-- Add your navbar content here -->
-</nav>
--- resources/views/layout/sidebar.blade.php
+++
@@ -1,3 +0,0 @@
-<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <!-- Add your sidebar content here -->
-</nav>
